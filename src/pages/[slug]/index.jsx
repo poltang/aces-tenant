4c8823f1a020ea8550f38fd4e6076fe6e1bd6a37
muslax <muslax@gmail.com>
2020-11-03 23:29:26 +0700

    license -> license?
    {license?.licenseName}

--- src/pages/[slug]/index.jsx
+++ src/pages/[slug]/index.jsx
@@ -53,8 +53,11 @@ export default function License({ license }) {
       </Head>
 
       <main className={styles.main}>
-        <h1 className="title sm">
+        {/* <h1 className="title sm">
           Welcome, {license.licenseName}!
+        </h1> */}
+        <h1 className="title sm">
+          Welcome, {license?.licenseName}!
         </h1>
 
         <FuncBox license={license}/>
